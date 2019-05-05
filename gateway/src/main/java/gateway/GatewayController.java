1e84d36545f11d11a9ffcd5d9b4a473747368d5c
Iván López <lopez.ivan@gmail.com>
2019-05-05 13:14:15 +0200

    Add Zipkin support

--- gateway/src/main/java/gateway/GatewayController.java
+++ gateway/src/main/java/gateway/GatewayController.java
@@ -2,6 +2,8 @@ package gateway;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.tracing.annotation.NewSpan;
+import io.micronaut.tracing.annotation.SpanTag;
 import io.reactivex.Single;
 
 @Controller("/api/${api.version:v1}")
@@ -13,8 +15,9 @@ public class GatewayController implements EncryptOperations {
         this.encryptClient = encryptClient;
     }
 
+    @NewSpan
     @Get("/encrypt/{text}")
-    public Single<MyMessage> encrypt(String text) {
+    public Single<MyMessage> encrypt(@SpanTag("plain-text") String text) {
         return encryptClient.encrypt(text);
     }
 }
