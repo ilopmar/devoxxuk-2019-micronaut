24ee0e1596d8d2696bc8a8eafc11814a2e713981
Iván López <lopez.ivan@gmail.com>
2019-05-05 12:57:59 +0200

    Add encrypt controller, HTTP client and test

---
+++ encrypt-service/src/test/groovy/encrypt/service/EncryptControllerSpec.groovy
@@ -0,0 +1,18 @@
+package encrypt.service
+
+import io.micronaut.test.annotation.MicronautTest
+import spock.lang.Specification
+
+import javax.inject.Inject
+
+@MicronautTest
+class EncryptControllerSpec extends Specification {
+
+    @Inject
+    EncryptClient encryptClient
+
+    void 'test encrypt controller'() {
+        expect:
+            encryptClient.encrypt('Hello Devoxx UK').getText() == 'KU xxoveD olleH'
+    }
+}
