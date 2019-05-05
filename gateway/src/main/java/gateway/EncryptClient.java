3900d175c6d7c52b061ecc3ab2e56080eec32e13
Iván López <lopez.ivan@gmail.com>
2019-05-05 13:10:14 +0200

    Add fallback support to encrypt client

--- gateway/src/main/java/gateway/EncryptClient.java
+++ gateway/src/main/java/gateway/EncryptClient.java
@@ -1,12 +1,8 @@
 package gateway;
 
-import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.annotation.Client;
-import io.reactivex.Single;
 
 @Client(id = "encrypt-service")
-public interface EncryptClient {
+public interface EncryptClient extends EncryptOperations {
 
-    @Get("/encrypt/{text}")
-    Single<MyMessage> encrypt(String text);
 }
